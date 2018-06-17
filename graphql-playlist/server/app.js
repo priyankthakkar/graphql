0d9236ff775603fffceb28f8c37cfe02c8fa3291
Priyank Thakkar <priyankthakkar@gmail.com>
2018-06-17 16:47:55 +1000

    Change Log: 1. A root query for graphql server is defined 2. a graphql schema is created and in place 3. we are yet to write code for retrieve data from the data source. Changes By : Priyank Thakkar

--- graphql-playlist/server/app.js
+++ graphql-playlist/server/app.js
@@ -3,13 +3,13 @@ const express = require('express');
 // our aim is to use graphql as a middleware, which will be
 // one supercharged endpoint to all incoming queries
 const graphqlHTTP = require('express-graphql');
-
+const schema = require('./schema/schema');
 const app = express();
 
 // graphql middleware - supercharged endpoint
 // this middleware depends on graphql schema, which define how are your data will be organzied
 app.use('/graphql', graphqlHTTP({
-
+	schema
 }));
 app.listen(4000, () => {
 	console.log('Application is listening at http://localhost:4000');
--- graphql-playlist/server/schema/schema.js
+++ graphql-playlist/server/schema/schema.js
@@ -1,22 +1,22 @@
 const graphql = require('graphql');
 
-const { GraphQLObjectType, GraphQLString } = graphql;
+const { GraphQLObjectType, GraphQLString, GraphQLSchema } = graphql;
 
 var data = [
 	{
-		"id": "",
+		"id": "1",
 		"name": "",
-		"genre": ""
+		"genre": "fiction"
 	},
 	{
-		"id": "",
+		"id": "2",
 		"name": "",
-		"genre": ""
+		"genre": "fiction"
 	},
 	{
-		"id": "",
+		"id": "3",
 		"name": "",
-		"genre": ""
+		"genre": "sci-fi"
 	}
 ];
 
@@ -26,5 +26,34 @@ const BookType = new GraphQLObjectType({
 		id: {type: GraphQLString},
 		name: {type: GraphQLString},
 		genre: {type: GraphQLString}
-	});
+	})
+});
+
+// We are defining a RootQuery here, where we mention
+// What is the type we are querying?
+// For e.g. here we querying BookType,
+// also we mention, that book should be queried based on id 
+// which is going to be of type String(GraphQLString to be precise)
+
+// same RootQuery will look like
+// book(id: '1') {
+// 	name
+// 	genre
+// }
+const RootQuery = new GraphQLObjectType({
+	name: 'RootQueryType',
+	fields: {
+		book: {
+			type: BookType,
+			args: { id: { type: GraphQLString } },
+			resolve(parent, args) {
+				// place code here to get data from db/datasource
+
+			}
+		}
+	}
+});
+
+module.exports = new GraphQLSchema({
+	query: RootQuery
 });
