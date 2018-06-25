bc1a6f5ee7900e409e93968f9526e3b880979a20
Priyank Thakkar <priyankthakkar@gmail.com>
2018-06-26 07:56:03 +1000

    Change Log: 1. Now we have an option to add a new book via UI 2. This also refreshes the exisiting list of books Changes By: Priyank Thakkar

---
+++ graphql-playlist/client/src/queries/queries.js
@@ -0,0 +1,30 @@
+import { gql } from 'apollo-boost';
+
+const getAuthorsQuery = gql`
+    {
+        authors {
+            id
+            name
+        }
+    }
+`;
+
+const getBooksQuery = gql`
+    {
+        books {
+            name
+            id
+        }
+    }
+`;
+
+const addBookMutation = gql`
+    mutation($name: String!, $genre: String!, $authorId: ID!) {
+        addBook(name: $name, genre: $genre, authorId: $authorId) {
+            id
+            name
+        }
+    }
+`;
+
+export { getAuthorsQuery, getBooksQuery, addBookMutation };
